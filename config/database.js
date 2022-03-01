a7bbbf0ab6b3abe1c789bcb43f8c7ac2da9a7bd4
javiern8410 <javier.nieve@despegar.com>
2022-03-01 16:18:47 -0300

    #Change database config

--- config/database.js
+++ config/database.js
@@ -1,20 +1,22 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: "mongoose",
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'tangud-strapi-api'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
+        host: env("DATABASE_HOST", process.env.MONGO_HOST),
+        srv: env.bool("DATABASE_SRV", true),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME", process.env.MONGO_DB),
+        username: env("DATABASE_USERNAME", process.env.MONGO_USER),
+        password: env("DATABASE_PASSWORD", process.env.MONGO_PASSW),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+        ssl: env.bool("DATABASE_SSL", true),
       },
     },
   },
 });
+
+// const URI =  'mongodb://javiern8410:Asdfghj123*@cluster0-shard-00-00-vbmcs.mongodb.net:27017,cluster0-shard-00-01-vbmcs.mongodb.net:27017,cluster0-shard-00-02-vbmcs.mongodb.net:27017/mern-tasks?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
