a26136cefe76142892ac377226ecae357fc31201
javiern8410 <javier.nieve@despegar.com>
2022-03-02 08:34:24 -0300

    #Fix security notification

--- config/database.js
+++ config/database.js
@@ -18,5 +18,3 @@ module.exports = ({ env }) => ({
     },
   },
 });
-
-// const URI =  'mongodb://javiern8410:Asdfghj123*@cluster0-shard-00-00-vbmcs.mongodb.net:27017,cluster0-shard-00-01-vbmcs.mongodb.net:27017,cluster0-shard-00-02-vbmcs.mongodb.net:27017/mern-tasks?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
